8f013ca2314aa807941e52c37e8e769329137f7f
zerotoasic <zerotoasic@zerotoasic.vm.com>
2024-05-29 02:17:12 +0200

    first commit

--- mag/tcl/antenna.tcl
+++
@@ -1,7 +0,0 @@
-set project [lindex $argv $argc-1]
-load $project.mag
-select top cell
-extract all
-antennacheck debug
-antennacheck
-quit -noprompt
--- mag/tt-analog-draw.tcl
+++ mag/tt-analog-draw.tcl
@@ -1,14 +1,13 @@
 # Important: before running this script, download the the .def file from
 # https://github.com/TinyTapeout/tt-support-tools/blob/tt06/def/analog/tt_block_1x2_pg_ana.def
-set project [lindex $argv $argc-1]
 
 def read tt_block_1x2_pg_ana.def
-cellname rename tt_um_template $project
+cellname rename tt_um_template tt_um_Saitama224_comp
 
 # VPWR
 box 100 500 250 22076
 paint met4
-label VPWR FreeSans 300 0 0 0 n met4
+label VPWR FreeSans met4
 port VPWR make n
 port VPWR use power
 port VPWR class bidirectional
@@ -17,12 +16,12 @@ port conn n s e w
 # VGND
 box 4900 500 5050 22076
 paint met4
-label VGND FreeSans 300 0 0 0 n met4
+label VGND FreeSans met4
 port VGND make n
 port VGND use ground
 port VGND class bidirectional
 port conn n s e w
 
-save
-
-quit -noprompt
+# Export
+gds write ../gds/tt_um_Saitama224_comp.gds
+lef write ../lef/tt_um_Saitama224_comp.lef -pinonly
